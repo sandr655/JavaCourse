191f065b00f6eac1afafcb783fb15b99341515b1
sandr655 <62062307+sandr655@users.noreply.github.com>
2021-12-10 16:33:25 +0300

    Попков_EX_4

--- Main.java
+++ Main.java
@@ -1,56 +1,111 @@
 package com.company;
 
 
-import java.util.Arrays;
+
+
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
+        int[][] arr  = {{1,2,3},{5,6,7},{9,10,11},{22,23,24}};
+//        Scanner scr = new Scanner(System.in);
+//
+//        int[][] arr = new int[3][2];
+//        for(int s = 0; s < arr.length; s++){
+//
+//            for (int k = 0; k < arr[s].length; k++){
+//                System.out.println("введите число");
+//                arr[s][k] = scr.nextInt();
+//            }
+//        }
+
+// 1
+        System.out.println("#1");
+        int col = 0;
+        int str = 0;
+
+        for(int s = 0; s < arr.length; s++){
+            int sum = 0;
+            col = 0;
+            str = str+1;
+            for (int k = 0; k < arr[s].length; k++){
+                sum = sum + arr[s][k];
+                col = col+1;
+            }
+            System.out.println("line "+(s+1)+" сумма "+ sum);
+        }
+// 2
+        System.out.println("#2");
+
+            for(int i = 0; i != col; i++){
+                int sum = 0;
+                for(int i2 = 0; i2 != str; i2++) {
+                    sum = sum + arr[i2][i];
+                }
+                System.out.println("Column "+(i+1)+" сумма "+sum);
+            }
+
+        // 4
+        System.out.println("\n"+"#4");
+        col = 0;
+        str = 0;
+
+        for(int s = 0; s < arr.length; s++){
+            int sum = 0;
+            col = 0;
+            str = str+1;
+            for (int k = 0; k < arr[s].length; k++){
+                sum = sum + arr[s][k];
+                col = col+1;
+            }
+            System.out.println("line "+(s+1)+" сумма "+ (sum/col));
+        }
+// 5
+        System.out.println("\n"+"#5");
+        List<Integer> arrList = new ArrayList();
+
+        for(int s = 0; s < arr.length; s++) {
+            str = str + 1;
+            for (int k = 0; k < arr[s].length; k++) {
+                arrList.add(arr[s][k]);
+            }
+        }
+
+            Integer[] arrArray = new Integer[arrList.size()];
+            Collections.reverse(arrList);
+            for (int i = 0; i < arrList.size(); i++) {
+                arrArray[i] = arrList.get(i);
+            }
+
+        System.out.println(Arrays.toString(arrArray));
+ // 3
+        System.out.println("\n"+"#3");
 
 
-        int[] arr = new int[]{3, 22, 1, 13, 4, 6, 16, 5};
+            int[][] arr2 = arr;
 
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] % 2 == 0) {
-                System.out.print(arr[i] + " ");
+            for (int s = 0; s < arr2.length; s++) {
+
+                for (int k = 0; k < arr2[s].length; k++) {
+                    if (arr2[s][k] % 2 == 0) {
+                        arr2[s][k] = 0;
+                    } else {
+                        arr2[s][k] = 1;
+                    }
+                }
+
+            }
+            System.out.print("новый массив ");
+            for (int s = 0; s < arr2.length; s++) {
+                System.out.println();
+                for (int k = 0; k < arr2[s].length; k++) {
+                    System.out.print(arr2[s][k] + " ");
+                }
             }
 
 
-        }
-        System.out.println();
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] > 9 && arr[i] < 100) {
-                System.out.print(arr[i] + " ");
-            }
-        }
-
-        int max = 0;
-
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] > max) {
-                max = arr[i];
-            }
-        }
-        System.out.println("\n" + max);
-
-        int sum = 0;
-        for(int i : arr){
-            sum += i;
-        }
-        System.out.println(sum);
-
-
-        int m = arr.length;
-        int[] arrP = new int[m];
-        m = arr.length - 1;
-        int i2 = 0;
-
-        for (int i = m; i!=-1; i--){
-            arrP[i2] = arr[m];
-            m = m-1;
-            i2++;
 
         }
-        System.out.println(Arrays.toString(arrP));
+
     }
-}
